1ebde5cd8b764f0aabd684d2e3c4d64fd49bbce2
pranavashish <pranavashish@gmail.com>
2023-05-08 16:48:34 +0530

    Learning @track and getter

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
    
@@ -8,4 +8,35 @@ export default class HelloWorld extends LightningElement {
     changeHandler(event){
         this.title = event.target.value;
     }
+
+    @track address={
+        city:'Melbourne',
+        postcode:3008,
+        country:'Austrailia'
+    }
+
+
+
+    trackHandler(event){
+        this.address.city = event.target.value;
+    }
+
+    // trackHandler1(event){
+    //     this.address = {...this.address,"city": event.target.value};
+    // }
+
+    users = ["Pranav", "Roushan", "Anil"];
+    num1 = 10;
+    num2 = 20;
+
+    get firstUser(){
+        return this.users[1].toUpperCase();
+    }
+
+    get sumOfNums(){
+        return this.num1 + this.num2;
+    }
+
+    
+    
 }
